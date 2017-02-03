f6a4a362c9ec47a33de031d356d630916f32adee
Vespertino <Vespertino@C6PC10>
2017-02-03 21:33:17 +0100

    cap3 terminado

--- src/Pixelpro/MisCursosBundle/Controller/DefaultController.php
+++ src/Pixelpro/MisCursosBundle/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Pixelpro\MisCursosBundle\Form\CursosType;
 use Pixelpro\MisCursosBundle\Entity\Cursos;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class DefaultController extends Controller {
 
@@ -45,15 +46,38 @@ class DefaultController extends Controller {
     }
 
     public function editarAction($id) {
-        return $this->render('PixelproMisCursosBundle:Default:editar.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $curso = $em->getRepository('PixelproMisCursosBundle:Cursos')->find($id);
+        $form = $this->createForm(CursosType::class, $curso, array('action' => $this->generateUrl('pixelpro_mis_cursos_actualizar', array("id" => $curso->getId())), 'method' => 'PUT'));
+        $form->add('save', SubmitType::class, array("label" => "Actualizar Curso"));
+        return $this->render('PixelproMisCursosBundle:Default:editar.html.twig', array('form' => $form->createView()));
     }
 
     public function actualizarAction(Request $request, $id) {
-        
+        $em = $this->getDoctrine()->getManager();
+        $curso = $em->getRepository('PixelproMisCursosBundle:Cursos')->find($id);
+        $form = $this->createForm(CursosType::class, $curso, array('action' => $this->generateUrl('pixelpro_mis_cursos_actualizar', array("id" => $curso->getId())), 'method' => 'PUT'));
+        $form->add('save', SubmitType::class, array("label" => "Actualizar Curso"));
+
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager(); //manejador de doctrine
+            $em->persist($curso); //crear una query
+            $em->flush(); //ejecutar query
+            $this->get('session')->getFlashBag()->add('notif', 'Curso actualizado correctamente!');
+            return $this->redirect($this->generateUrl('pixelpro_mis_cursos_mostrar', array('id' => $curso->getId()))); //redirigimos a la vista para meter mas cursos
+        }
+        $this->get('session')->getFlashBag()->add('notif', 'Curso NO actualizado correctamente!');
+        return $this->render('PixelproMisCursosBundle:Default:editar.html.twig', array('form' => $form->createView()));
     }
 
     public function eliminarAction(Request $request, $id) {
-        
+        $em = $this->getDoctrine()->getManager();
+        $curso = $em->getRepository('PixelproMisCursosBundle:Cursos')->find($id);
+        $em->remove($curso);
+        $em->flush();
+        $this->get('session')->getFlashBag()->add('notif', 'Curso eliminado correctamente!');
+        return $this->redirect($this->generateUrl('pixelpro_mis_cursos_homepage'));
     }
 
 }
